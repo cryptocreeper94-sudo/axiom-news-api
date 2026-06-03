b2be3197e4971730f6abb2fd8c4e8cac19beac7d
cryptocreeper94 <jason@livfitoken.com>
2026-06-03 00:35:32 -0500

    Fix database cross-region connection timeout

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+process.env.DATABASE_URL = "postgresql://lume_cortex_user:lxKEqdUQcLDOr1VIiLiSxIFb2sqPDtsE@dpg-d7p4u7pkh4rs73btif0g-a.ohio-postgres.render.com/lume_cortex?sslmode=require";
+
 const express = require('express');
 const cors = require('cors');
 const cron = require('node-cron');
